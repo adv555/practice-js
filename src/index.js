2e7f76d55215961f7ea75d934b89a7ffa16f9fc5
Olena Boldysheva <76661298+adv555@users.noreply.github.com>
2021-07-27 00:51:17 +0300

    added solutions V1 & V2

--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
-import './sass/main.scss';
 import './js/changeTheme';
-import menuCards from './menu.json';
-import menuCardTpl from './templates/menu-item.hbs';
+import menu from './menu.json';
+import creatMenuMarkup from './templates/menu-item.hbs';
 
 // =========== создает разметку по шаблону ===========//
 
 const menuContainer = document.querySelector('.js-menu');
-const menuMarkup = createMenuMarkup(menuCards);
-
-function createMenuMarkup(menuCards) {
-  return menuCards.map(menuCardTpl).join('');
-}
-
-menuContainer.insertAdjacentHTML('afterbegin', menuMarkup);
+menuContainer.insertAdjacentHTML('afterbegin', creatMenuMarkup(menu));
--- src/js/changeTheme.js
+++ src/js/changeTheme.js
@@ -1,35 +1,64 @@
+// ======================== ВАРИАНТ 1 ======================== //
+
 const Theme = {
   LIGHT: 'light-theme',
   DARK: 'dark-theme',
 };
 
 const refs = {
-  switch: document.getElementById('theme-switch-toggle'),
-  body: document.querySelector('body'),
+  switcher: document.querySelector('#theme-switch-toggle'),
+  body: document.body,
 };
 
-// ============ Upload Current THEME ============ //
-const uploadCurrentTheme = () => {
-  refs.switch.checked = JSON.parse(localStorage.getItem('Checked'));
-  //   console.log(refs.switch.checked);
-
-  if (refs.switch.checked) {
-    refs.body.classList.remove(Theme.LIGHT);
-    refs.body.classList.add(Theme.DARK);
-  }
-  if (!refs.switch.checked) {
-    refs.body.classList.remove(Theme.DARK);
-    refs.body.classList.add(Theme.LIGHT);
-  }
-};
+document.addEventListener('change', changeTheme);
 
 // ============ Save Current Theme  ============ //
-const changeTheme = () => {
-  localStorage.setItem('Checked', refs.switch.checked);
-  uploadCurrentTheme();
-};
+function changeTheme({ target: { checked } }) {
+  checked ? toggledTheme(Theme.DARK, Theme.LIGHT) : toggledTheme(Theme.LIGHT, Theme.DARK);
+}
+// ============ Upload Current THEME ============ //
+(function themeDefault() {
+  refs.body.classList.add(
+    localStorage.getItem('theme') ? localStorage.getItem('theme') : Theme.LIGHT,
+  );
+  refs.switcher.checked = localStorage.getItem('theme') === Theme.DARK;
+})();
 
-// ============ Listeners============ //
+function toggledTheme(add, rem) {
+  refs.body.classList.add(add);
+  refs.body.classList.replace(rem, add);
+  localStorage.setItem('theme', add);
+}
 
-document.addEventListener('DOMContentLoaded', uploadCurrentTheme());
-refs.switch.addEventListener('change', changeTheme);
+// ======================== ВАРИАНТ 2 ======================== //
+
+// (function () {
+//   const Theme = {
+//     LIGHT: 'light-theme',
+//     DARK: 'dark-theme',
+//   };
+
+//   const refs = {
+//     menuList: document.querySelector('.js-menu'),
+//     switcher: document.querySelector('#theme-switch-toggle'),
+//     body: document.body,
+//   };
+//   const state = JSON.parse(localStorage.getItem('theme'));
+//   refs.body.classList.add(state?.theme ? state.theme : Theme.LIGHT);
+//   refs.switcher.checked = state?.checked;
+//   refs.menuList.innerHTML = createMenuMarkup(menu);
+//   refs.switcher.addEventListener('change', changeTheme);
+
+//   function changeTheme({ target: { checked } }) {
+//     checked ? toggleTheme(Theme.DARK, Theme.LIGHT) : toggleTheme(Theme.LIGHT, Theme.DARK);
+//   }
+
+//   function toggleTheme(add, rem) {
+//     refs.body.classList.replace(rem, add);
+//     const state = {
+//       theme: add,
+//       checked: add === Theme.DARK,
+//     };
+//     localStorage.setItem('theme', JSON.stringify(state));
+//   }
+// })();
