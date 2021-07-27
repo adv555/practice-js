76c41614902b25058618a1282df4108ad78d51cd
Olena Boldysheva <76661298+adv555@users.noreply.github.com>
2021-07-27 12:55:02 +0300

    added list

--- src/index.js
+++ src/index.js
@@ -1,8 +1,1 @@
-import './js/changeTheme';
-import menu from './menu.json';
-import creatMenuMarkup from './templates/menu-item.hbs';
-
-// =========== создает разметку по шаблону ===========//
-
-const menuContainer = document.querySelector('.js-menu');
-menuContainer.insertAdjacentHTML('afterbegin', creatMenuMarkup(menu));
+console.log('Test log');
